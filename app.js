9d8ffc3345dd3966b435645b056842383a8c554b
Yadegar <jalaldgr@gmail.com>
2023-05-15 19:12:09 +0330

    add body parser to catch req.body

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('server works functionally')
