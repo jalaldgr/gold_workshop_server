9dfdae737f3196ea989164fe3dc9b37940fd740e
Yadegar <jalaldgr@gmail.com>
2023-05-16 14:33:09 +0330

    rename models

--- src/helper/db.js
+++ src/helper/db.js
@@ -8,11 +8,11 @@ const connectionOptions = {
 mongoose.connect(process.env.MONGODB_URI, {});
 mongoose.Promise = global.Promise;
 module.exports = {
-    Admin: require('../models/admin'),
-    Designer:require('../models/designer'),
-    Workshop1:require('../models/workshop1'),
-    Workshop2:require('../models/workshop2'),
-    Order:require('../models/order')
+    Admin: require('../models/adminModel'),
+    Designer:require('../models/designerModel'),
+    Workshop1:require('../models/workshop1Model'),
+    Workshop2:require('../models/workshop2Model'),
+    Order:require('../models/orderModel')
 
 
 
