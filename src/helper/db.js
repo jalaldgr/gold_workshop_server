9ed382b0dda610742f5f8a47044a2647f6a43031
Yadegar <jalaldgr@gmail.com>
2023-05-16 10:40:06 +0330

    add order, workshop1, workshop2, designer models

--- src/helper/db.js
+++ src/helper/db.js
@@ -9,6 +9,10 @@ mongoose.connect(process.env.MONGODB_URI, {});
 mongoose.Promise = global.Promise;
 module.exports = {
     Admin: require('../models/admin'),
+    Designer:require('../models/designer'),
+    Workshop1:require('../models/workshop1'),
+    Workshop2:require('../models/workshop2'),
+    Order:require('../models/order')
 
 
 
---
+++ src/models/order.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    clientFullName: { type: String , required: true},
+    plateName: { type: String },
+    description: { type: String },
+    image: { type: String },
+    code:{type:String},
+    weight:{type:String},
+    status:{type:String,enum: ['pending', 'discarded', 'inDesign','inWorkshop1','inWorkshop2','completed','completeDesign','CompleteWorkshop1','completeWorkshop2' ]},
+    workshop1fullName:{type:String},
+    workshop1Id:{type:String},
+    workshop1File:{type:String},
+    workshop2fullName:{type:String},
+    workshop2Id:{type:String},
+    workshop2File:{type:String},
+    designerFullName:{type:String},
+    designerId:{type:String},
+    designerFile:{type:String},
+    createdDate: { type: Date, default: Date.now },
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.hash;
+    }
+});
+
+module.exports = mongoose.model('Order', schema);
