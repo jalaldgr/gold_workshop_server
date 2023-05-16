924fea26ebcdb4b17e15f524b34484f42d0c44e2
Yadegar <jalaldgr@gmail.com>
2023-05-16 11:36:42 +0330

    add some routes to admin router, to register users and create new order

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/adminController")
+const designerController = require("../controllers/designerController")
+const workshop1Controller = require("../controllers/workshop1Controller")
+const workshop2Controller = require("../controllers/workshop2Controller")
+const orderController = require("../controllers/orderController")
 
 
 router.post('/login', controller.postLogin)
 router.post('/register',controller.register)
+router.post('/register-designer',designerController.register)
+router.post('/register-workshop1',workshop1Controller.register)
+router.post('/register-workshop2',workshop2Controller.register)
+router.post('/create-order',orderController.create)
+
+
 
 
 router.get('/', (req, res) => {
