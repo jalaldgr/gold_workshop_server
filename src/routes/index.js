3f2543d508778f41c6b691c80c67f4c2c37f459c
Yadegar <jalaldgr@gmail.com>
2023-05-16 11:33:04 +0330

    add workshop1, workshop2 and designer subRoutes to main index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const adminRouter = require('./admin')
+const workshop1Router = require("./workshop1Router")
+const workshop2Router = require("./workshop2Router")
+const designerRouter = require("./designerRouter")
+
 
 
 router.use('/admin',adminRouter)
+router.use("/workshop1",workshop1Router)
+router.use("/workshop2",workshop2Router)
+router.use("/designer",designerRouter)
 router.get('/', (req, res) => {
     res.send('server works functionally')
 })
