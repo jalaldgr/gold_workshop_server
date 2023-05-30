aa323d7d703d8c83cfafa979ede0109a533425c5
Jalal Yadegar <jalaldgr@gmail.com>
2023-05-30 19:02:11 +0330

    allow all extensions files to upload

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -19,10 +19,10 @@ const storage = multer.diskStorage({
 const upload = multer(
     { storage: storage ,
         fileFilter: function (req, file, callback) {
-            var ext = path.extname(file.originalname);
-            if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-                return callback(new Error('Only images are allowed'))
-            }
+            // var ext = path.extname(file.originalname);
+            // if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            //     return callback(new Error('Only images are allowed'))
+            // }
             callback(null, true)
         },
         limits:{
