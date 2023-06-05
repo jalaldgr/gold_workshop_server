62b845609925c268019f98ceeefb3d903d377cf7
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-05 20:42:16 +0330

    add endpoints for designer

--- src/routes/designerRouter.js
+++ src/routes/designerRouter.js
@@ -1,12 +1,38 @@
 const express = require("express")
 const router = express.Router()
 const designerController = require("../controllers/designerController")
+const orderController = require("../controllers/orderController");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix+"-"+file.originalname)
+    }
+})
+const upload = multer(
+    { storage: storage ,
+        fileFilter: function (req, file, callback) {
+            // var ext = path.extname(file.originalname);
+            // if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            //     return callback(new Error('Only images are allowed'))
+            // }
+            callback(null, true)
+        },
+        limits:{
+            fileSize: 4096 * 1024
+        }
+    })
 
 
 router.post('/login', designerController.postLogin)
 router.get("/get-designer/:id",designerController.getDesignerById)
 router.post("/update-designer/:id",designerController.updateDesignerById)
-
+router.get("/get-all-pending-orders/:id/:status/",orderController.getAllPendingOrdersById)
+router.post("/complete-order/:id/",orderController.postCompleteOrder)
+router.post("/send-file/:id/:user/",upload.fields([{name:'designerFile'}]),orderController.postSendFile)
 
 router.get('/', (req, res) => {
     res.send('designer works functionally')
