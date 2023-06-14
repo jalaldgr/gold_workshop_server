e88b4bd416fd137e51502c9a11bcf70e3346d784
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-14 16:40:34 +0330

    update port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express()
-const port = 3000
+const port = 3001
 const routes = require('./src/routes/indexRouter')
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
