d7f756b1fda10ae63f0eb3936b68aceebe06dcbf
Jalal Yadegar <jalaldgr@gmail.com>
2023-06-19 18:54:25 +0330

    update designer model

--- src/models/designerModel.js
+++ src/models/designerModel.js
@@ -6,6 +6,7 @@ const schema = new Schema({
     hash: { type: String, required: true },
     fullName: { type: String, required: true },
     createdDate: { type: Date, default: Date.now },
+    isAdmin: {type:String,default: "no"},
     role:{type:String,required:true,default:"Designer"}
 });
 
