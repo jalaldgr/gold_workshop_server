12234ae56e06eddf591566a47f1b458bd78fa26d
Jalal Yadegar <jalaldgr@gmail.com>
2023-07-10 08:17:12 +0330

    fix table2 default value

--- src/models/tableModel.js
+++ src/models/tableModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const moment = require('moment'); // require
 const schema = new Schema({
     table1:{type:String,default:`{"row":[1],"other":[0],"consumption_load":[0],"consumptions_load_and_other":[0],"after_melt":[0],"difference":[0],"after_paging":[0],"final_difference":[0]}`},
-    table2:{type:String,default:`{"row":[0,2,3],"description":["مانده روز قبل","کسر برش","کسر ذوب"],"import":[0,0,0],"export":[0,0,0],"final_balance":[0,0,0],"real_balance":[0,0,0],"balance":[0,0,0],"difference":[0,0,0]}`},
+    table2:{type:String,default:`{"row":[1,2,3],"description":["مانده روز قبل","کسر برش","کسر ذوب"],"import":[0,0,0],"export":[0,0,0],"final_balance":[0,0,0],"real_balance":[0,0,0],"balance":[0,0,0],"difference":[0,0,0]}`},
     table3:{type:String,default:`{"description":["متفرقه"],"other":[0],"ring":[0],"wire":[0],"ball":[0],"solder":[0],"chain":[0],"piece_chain":[0],"lock":[0],"work_made":[0],"sum":[0]}`},
     table41:{type:String,default: `{"row":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],"description":["متفرقه","حلقه","مفتول","گوی","لحیم","زنجیر","تکه زنجیر","مدبر","کارساخت","کارساخت","کارساخت","کارساخت","متفرقه","متفرقه","متفرقه","متفرقه","متفرقه","متفرقه","متفرقه","متفرقه","سرسنجاق"],"import_weight":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"summary":["","","","","","","","","","","","","","","","","","","","",""]}`},
     table42:{type:String,default:`{"description":["متفرقه","حلقه","مفتول","گوی","لحیم","زنجیر","تکه زنجیر","مدبر","کارساخت","سرسنجاق"],"real_balance":[0,0,0,0,0,0,0,0,0,0],"system_balance":[0,0,0,0,0,0,0,0,0,0],"difference":[0,0,0,0,0,0,0,0,0,0],"summary":["","","","","","","","","",""]}`},
